3a83648ac850663ac3d3d57bdb6978de89d5eb0d
Curren Taber <currentaber@gmail.com>
2018-11-05 11:54:32 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -3,7 +3,7 @@
 # Curren Taber
 echo "Enter a username (only lower case letters, digits, and the underscore character; it must start with a lower case letter; at least 3 char; max 12 char): "
 read username
-while echo $username | egrep -v "^[_a-z0-9]{3,12}$" > /dev/null 2>&1
+while echo $username | egrep -v "^[a-z][_a-z0-9]{3,12}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username (only lower case letters, digits, and the underscore character; it must start with a lower case letter; at least 3 char; max 12 char)"
 	echo "Enter a username: "
